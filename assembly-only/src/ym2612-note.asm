3e2184c9e160cde24f2ac9845acac706e6cd4b00
rhargreaves <robert@roberthargreaves.net>
2018-01-07 19:45:05 +0000

    Add orange background to give feedback that ROM loaded

--- assembly-only/src/ym2612-note.asm
+++ assembly-only/src/ym2612-note.asm
@@ -23,6 +23,24 @@ hblank_counter	equ 0x00FF0000          ; Start of RAM
 vblank_counter	equ (hblank_counter+SizeLong)
 audio_clock	equ (vblank_counter+SizeLong)
 
+Palette:
+   dc.w 0x0000 ; Colour 0 - Transparent
+   dc.w 0x000E ; Colour 1 - Red
+   dc.w 0x00E0 ; Colour 2 - Green
+   dc.w 0x0E00 ; Colour 3 - Blue
+   dc.w 0x0000 ; Colour 4 - Black
+   dc.w 0x0EEE ; Colour 5 - White
+   dc.w 0x00EE ; Colour 6 - Yellow
+   dc.w 0x008E ; Colour 7 - Orange
+   dc.w 0x0E0E ; Colour 8 - Pink
+   dc.w 0x0808 ; Colour 9 - Purple
+   dc.w 0x0444 ; Colour A - Dark grey
+   dc.w 0x0888 ; Colour B - Light grey
+   dc.w 0x0EE0 ; Colour C - Turquoise
+   dc.w 0x000A ; Colour D - Maroon
+   dc.w 0x0600 ; Colour E - Navy blue
+   dc.w 0x0060 ; Colour F - Dark green
+
 HBlankInterrupt:
 	addi.l	#0x1, hblank_counter	; Increment hinterrupt counter
 	rte
@@ -58,6 +76,18 @@ WaitFrames:
 	jmp     @Wait              ; Try again
 	
 	@End:
+	rts
+
+SetPurpleBackground:
+	move.l #0x40000003, VDPCtrlPort	; we’re about to write data to VRAM address 0xC000
+	move.w #0x8F02, VDPCtrlPort  	; Set autoincrement to 2 bytes
+	move.l #0xC0000003, VDPCtrlPort 	; Set up VDP to write to CRAM address 0x0000
+	lea Palette, a0          	; Load address of Palette into a0
+	move.l #0x07, d0         	; 32 bytes of data (8 longwords, minus 1 for counter) in palette
+	@Loop:
+	move.l (a0)+, 0x00C00000 	; Move data to VDP data port, and increment source address
+	dbra d0, @Loop
+	move.w #0x8707, VDPCtrlPort  	; Set background colour to palette 0, colour 8
 	rts
 
 WaitFmReady:
@@ -120,6 +150,7 @@ PlayYm2612Note:
 __main:
 	move.w	#0x8F02, VDPCtrlPort     ; Set autoincrement to 2 bytes
 	move.w	#0x8708, VDPCtrlPort     ; Set background colour light blue (palette 0, colour 8)
+	jsr	SetPurpleBackground
 	jsr	PlayYm2612Note
 
 GameLoop:
