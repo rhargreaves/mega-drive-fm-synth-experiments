c0a8f53ccc9562985d548049a775068c6f5f6a26
rhargreaves <robert@roberthargreaves.net>
2018-01-08 01:03:02 +0000

    Play chord in PSG with C

--- src/main.c
+++ src/main.c
@@ -5,13 +5,21 @@ int main()
     VDP_setTextPalette(PAL1);
     VDP_drawText("FM & PSG Test", 13, 0);
 
-    PSG_write(0b10001110);
-    PSG_write(0b00001111);
-    PSG_setEnvelope(0, 0);
+    PlayChord();
 
 	while(1)
     {
         VDP_waitVSync();
     }
     return (0);
+}
+
+void PlayChord()
+{
+    PSG_setFrequency(0, 440);
+    PSG_setEnvelope(0, 1);
+    PSG_setFrequency(1, 554);
+    PSG_setEnvelope(1, 1);
+    PSG_setFrequency(2, 659);
+    PSG_setEnvelope(2, 1);
 }
