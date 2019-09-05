434433d3eb05dfc725bd9c2f9058d47c0308ab63
rhargreaves <robert@roberthargreaves.net>
2019-09-05 20:21:20 +0100

    Fix algorithm operator order

--- src/ui_display.c
+++ src/ui_display.c
@@ -1,7 +1,7 @@
-#include <genesis.h>
-#include <synth.h>
 #include <channel.h>
+#include <genesis.h>
 #include <stdbool.h>
+#include <synth.h>
 #include <ui_display.h>
 
 #define PAL_HEADING PAL1
@@ -33,33 +33,22 @@ static void printFms(u16 index, u16 x, u16 y);
 static void printMultiple(u16 index, u16 x, u16 y);
 static void printParameter(u16 index, u16 heading_x, u16 value_x, u16 y, u8 selection);
 
-static FmParameterUi globalParameterUis[] = {
-    {"Globl LFO", 1, NULL, printOnOff},
-    {"Freq", 1, NULL, printLFOFreq}};
+static FmParameterUi globalParameterUis[] = {{"Globl LFO", 1, NULL, printOnOff},
+                                             {"Freq", 1, NULL, printLFOFreq}};
 
 static FmParameterUi fmParameterUis[] = {
-    {"Note", 2, NULL, printNote},
-    {"Freq #", 4, NULL, NULL},
-    {"Octave", 1, NULL, NULL},
-    {"Algorithm", 1, NULL, printAlgorithm},
-    {"Feedback", 1, NULL, NULL},
-    {"LFO AMS", 1, NULL, printAms},
-    {"LFO FMS", 1, NULL, printFms},
-    {"Stereo", 1, NULL, printStereo}};
+    {"Note", 2, NULL, printNote},   {"Freq #", 4, NULL, NULL},
+    {"Octave", 1, NULL, NULL},      {"Algorithm", 1, NULL, printAlgorithm},
+    {"Feedback", 1, NULL, NULL},    {"LFO AMS", 1, NULL, printAms},
+    {"LFO FMS", 1, NULL, printFms}, {"Stereo", 1, NULL, printStereo}};
 
 static OperatorParameterUi opParameterUis[] = {
-    {"Detune", 1, NULL},
-    {"Multiple", 2, printMultiple},
-    {"Total Lvl", 3, NULL},
-    {"Rate Scl", 1, NULL},
-    {"Atck Rate", 2, NULL},
-    {"Ampl Mode", 1, NULL},
-    {"Decay 1", 2, NULL},
-    {"Decay 2", 2, NULL},
-    {"Sub Level", 2, NULL},
-    {"Rel Rate", 2, NULL},
-    {"Octave", 1, NULL},
-    {"Freq #", 4, NULL}};
+    {"Detune", 1, NULL},    {"Multiple", 2, printMultiple},
+    {"Total Lvl", 3, NULL}, {"Rate Scl", 1, NULL},
+    {"Atck Rate", 2, NULL}, {"Ampl Mode", 1, NULL},
+    {"Decay 1", 2, NULL},   {"Decay 2", 2, NULL},
+    {"Sub Level", 2, NULL}, {"Rel Rate", 2, NULL},
+    {"Octave", 1, NULL},    {"Freq #", 4, NULL}};
 
 static bool drawUi = false;
 
@@ -80,10 +69,7 @@ void display_draw(Channel *chan, u8 selection)
     VDP_setTextPalette(PAL0);
 }
 
-void display_requestUiUpdate(void)
-{
-    drawUi = true;
-}
+void display_requestUiUpdate(void) { drawUi = true; }
 
 void display_updateUiIfRequired(Channel *chan, u8 selection)
 {
@@ -96,7 +82,8 @@ void display_updateUiIfRequired(Channel *chan, u8 selection)
 
 static void printGlobalParameters(u8 selection)
 {
-    printParameter(0, LEFT_MARGIN, FM_PARAMETERS_VALUE_COLUMN, GLOBAL_PARAMETERS_TOP_ROW, selection);
+    printParameter(0, LEFT_MARGIN, FM_PARAMETERS_VALUE_COLUMN, GLOBAL_PARAMETERS_TOP_ROW,
+                   selection);
     printParameter(1, 23, 28, GLOBAL_PARAMETERS_TOP_ROW, selection);
 }
 
@@ -113,10 +100,7 @@ static void printParameter(u16 index, u16 heading_x, u16 value_x, u16 y, u8 sele
     }
     else
     {
-        printNumber(value,
-                    p->minSize,
-                    value_x,
-                    y);
+        printNumber(value, p->minSize, value_x, y);
     }
 }
 
@@ -133,16 +117,11 @@ static void printFmParameters(Channel *chan, u8 selection)
         u16 value = channel_parameterValue(chan, index);
         if (p->printFunc != NULL)
         {
-            p->printFunc(value,
-                         FM_PARAMETERS_VALUE_COLUMN,
-                         row);
+            p->printFunc(value, FM_PARAMETERS_VALUE_COLUMN, row);
         }
         else
         {
-            printNumber(value,
-                        p->minSize,
-                        FM_PARAMETERS_VALUE_COLUMN,
-                        row);
+            printNumber(value, p->minSize, FM_PARAMETERS_VALUE_COLUMN, row);
         }
     }
 }
@@ -171,7 +150,8 @@ static void printOperatorHeader(Operator *op)
     VDP_setTextPalette(PAL_HEADING);
     char opHeader[4];
     sprintf(opHeader, "Op%u", op->opNumber + 1);
-    VDP_drawText(opHeader, OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN, OPERATOR_TOP_ROW);
+    VDP_drawText(opHeader, OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN,
+                 OPERATOR_TOP_ROW);
     VDP_setTextPalette(PAL0);
 }
 
@@ -180,7 +160,8 @@ static void printOperator(Operator *op, u8 selection)
     for (u16 index = 0; index < OPERATOR_PARAMETER_COUNT; index++)
     {
         u16 row = index + OPERATOR_TOP_ROW + 1;
-        if (op->chanNumber != 2 && (index == OP_PARAMETER_CH3_FREQ || index == OP_PARAMETER_CH3_OCTAVE))
+        if (op->chanNumber != 2 &&
+            (index == OP_PARAMETER_CH3_FREQ || index == OP_PARAMETER_CH3_OCTAVE))
         {
             VDP_clearText(LEFT_MARGIN, row, 40);
             continue;
@@ -192,7 +173,8 @@ static void printOperator(Operator *op, u8 selection)
             VDP_drawText(opParameterUis[index].name, LEFT_MARGIN, row);
             VDP_setTextPalette(PAL0);
         }
-        if (selection - FM_PARAMETER_COUNT == GLOBAL_PARAMETER_COUNT + index + op->opNumber * OPERATOR_PARAMETER_COUNT)
+        if (selection - FM_PARAMETER_COUNT ==
+            GLOBAL_PARAMETER_COUNT + index + op->opNumber * OPERATOR_PARAMETER_COUNT)
         {
             VDP_setTextPalette(PAL_SELECTION);
         }
@@ -200,15 +182,12 @@ static void printOperator(Operator *op, u8 selection)
         if (opUi->printFunc != NULL)
         {
             opUi->printFunc(operator_parameterValue(op, index),
-                         OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN,
-                         row);
+                            OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN, row);
         }
         else
         {
-            printNumber(operator_parameterValue(op, index),
-                    opParameterUis[index].minSize,
-                    OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN,
-                    row);
+            printNumber(operator_parameterValue(op, index), opParameterUis[index].minSize,
+                        OPERATOR_VALUE_WIDTH * op->opNumber + OPERATOR_VALUE_COLUMN, row);
         }
         VDP_setTextPalette(PAL0);
     }
@@ -216,7 +195,8 @@ static void printOperator(Operator *op, u8 selection)
 
 static void printLFOFreq(u16 index, u16 x, u16 y)
 {
-    const char TEXT[][8] = {"3.98Hz", "5.56Hz", "6.02Hz", "6.37Hz", "6.88Hz", "9.63Hz", "48.1Hz", "72.2Hz"};
+    const char TEXT[][8] = {"3.98Hz", "5.56Hz", "6.02Hz", "6.37Hz",
+                            "6.88Hz", "9.63Hz", "48.1Hz", "72.2Hz"};
     printLookup(index, TEXT[index], x, y);
 }
 
@@ -240,7 +220,8 @@ static void printStereo(u16 index, u16 x, u16 y)
 
 static void printAlgorithm(u16 index, u16 x, u16 y)
 {
-    const char TEXT[][10] = {"1*2*3*4", "(1+2)*3*4", "(1+2*3)*4", "(1*2+3)*4", "1*3+2*4", "1*(2+3+4)", "1*2+3+4", "1+2+3+4"};
+    const char TEXT[][10] = {"1*3*2*4", "(1+3)*2*4", "(1+3*2)*4", "(1*3+2)*4",
+                             "1*3+2*4", "1*(2+3+4)", "1*3+2+4",   "1+2+3+4"};
     printLookup(index, TEXT[index], x, y);
 }
 
@@ -259,7 +240,7 @@ static void printFms(u16 index, u16 x, u16 y)
 static void printMultiple(u16 index, u16 x, u16 y)
 {
     char buffer[4];
-    if(index == 0)
+    if (index == 0)
     {
         strcpy(buffer, ".5");
     }
